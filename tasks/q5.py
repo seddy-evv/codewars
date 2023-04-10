94a459258416cd030daa3737096ca5fb6151a8ae
Anatol <seddboom@gmail.com>
2023-04-10 20:49:01 +0300

    add tasl to q5.py

--- tasks/q5.py
+++ tasks/q5.py
@@ -51,5 +51,40 @@ def remov_nb(n):
     return list_low + list_high
 
 
+"""The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in 
+a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that 
+fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+The following are examples of expected output values:
+
+rgb(255, 255, 255) # returns FFFFFF
+rgb(255, 255, 300) # returns FFFFFF
+rgb(0,0,0) # returns 000000
+rgb(148, 0, 211) # returns 9400D3"""
+
+
+def rgb(r, g, b):
+    def dec_to_hexa(n):
+        if n < 0:
+            n = 0
+        elif n > 255:
+            n = 255
+
+        hex_str = hex(n).replace("0x", "")
+        if len(hex_str) == 1:
+            hex_str = "0" + hex_str
+
+        return hex_str.upper()
+
+    hex_code = ""
+    hex_code += dec_to_hexa(r)
+    hex_code += dec_to_hexa(g)
+    hex_code += dec_to_hexa(b)
+    return hex_code
+
+
 if __name__ == '__main__':
     print(remov_nb(26))
+    print(rgb(255, 255, 255))
